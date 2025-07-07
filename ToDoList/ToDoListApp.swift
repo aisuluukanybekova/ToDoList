04597884f004f83ea98bbe5c2f0162ff3eee0851
Aisuluu Kanybekova <kanybekovaaisuluu8@gmail.com>
2025-07-07 16:51:48 +0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Aisuluu Kanybekova  on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
